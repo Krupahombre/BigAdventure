236a816e153f4c231679ebc6c52bf982ca0d7c97
koxx4 <piotr815@poczta.fm>
2022-01-31 21:25:14 +0100

    Added initial implementation of ControlPanel

--- src/main/java/bigAdventure/gui/ControlPanel.java
+++ src/main/java/bigAdventure/gui/ControlPanel.java
@@ -1,6 +1,26 @@
 package bigAdventure.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ControlPanel extends JPanel {
+
+    private final JButton fightButton;
+    private final JButton defenseButton;
+
+    public ControlPanel() {
+        fightButton = new JButton("Fight");
+        defenseButton = new JButton("Defend yourself");
+        initializeComponent();
+    }
+
+    private void initializeComponent(){
+        this.setVisible(true);
+        this.setLayout(new FlowLayout(FlowLayout.LEFT, 30, 20));
+        this.setBorder(BorderFactory.createTitledBorder("Controls"));
+        this.add(fightButton);
+        this.add(defenseButton);
+    }
+
+
 }
